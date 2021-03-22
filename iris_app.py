890ebd5dbe90af19c037aecdf19439d20d93de83
Daniel O'Keefe <33761482+DanOKeefe@users.noreply.github.com>
2021-03-22 04:11:15 -0400

    Add files via upload

---
+++ iris_app.py
@@ -0,0 +1,126 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+import plotly.graph_objects as go
+import plotly.express as px
+from tensorflow import keras
+
+# Read data
+df = pd.read_csv('https://gist.githubusercontent.com/netj/8836201/raw/6f9306ad21398ea43cba4f7d537619d0e07d5ae3/iris.csv')
+
+# Clean data
+X = df.iloc[:, :4]
+y = np.zeros(shape=(X.shape[0], 3))
+
+for i, val in enumerate(df['variety']):
+    if val=='Virginica':
+        y[i,:] = np.array([1, 0, 0])
+    elif val=='Versicolor':
+        y[i,:] = np.array([0, 1, 0])
+    elif val=='Setosa':
+        y[i,:] = np.array([0, 0, 1])
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=100)
+
+# Scale data
+scaler = StandardScaler()
+scaler.fit(X_train)
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
+
+# Run PCA
+pca = PCA(2)
+X_pca = pca.fit(X).transform(X)
+df_pca = pd.DataFrame(pca.transform(X))
+df_pca.columns = ['PC1', 'PC2']
+df_pca = pd.concat([df_pca, df['variety']], axis=1)
+
+# Load model
+model = keras.models.load_model('iris_model')
+
+# App title and description
+st.title('Iris Flower Classifier')
+st.markdown("""
+Predict the species of an Iris flower using sepal and petal measurements.
+""")
+
+# Define components for the sidebar
+st.sidebar.header('Input Features')
+sepal_length = st.sidebar.slider(
+    label='Sepal Length',
+    min_value=df['sepal.length'].min(),
+    max_value=df['sepal.length'].max(),
+    value=round(df['sepal.length'].mean(), 1),
+    step=0.1)
+sepal_width = st.sidebar.slider(
+    label='Sepal Width',
+    min_value=df['sepal.width'].min(),
+    max_value=df['sepal.width'].max(),
+    value=round(df['sepal.width'].mean(), 1),
+    step=0.1)
+petal_length = st.sidebar.slider(
+    label='Petal Length',
+    min_value=df['petal.length'].min(),
+    max_value=df['petal.length'].max(),
+    value=round(df['petal.length'].mean(), 1),
+    step=0.1)
+petal_width = st.sidebar.slider(
+    label='Petal Width',
+    min_value=df['petal.width'].min(),
+    max_value=df['petal.width'].max(),
+    value=round(df['petal.width'].mean(), 1),
+    step=0.1)
+
+# Scale the user inputs
+X_scaled = scaler.transform([[sepal_length, sepal_width, petal_length, petal_width]])
+
+# Run input through the model
+y_pred = model.predict(X_scaled)
+df_pred = pd.DataFrame({
+    'Species': ['Virginica', 'Versicolor', 'Setosa'],
+    'Confidence': y_pred.flatten()
+})
+X_scaled = pd.DataFrame(X_scaled)
+X_scaled.columns = [
+    'Sepal Length Scaled',
+    'Sepal Width Scaled',
+    'Petal Length Scaled',
+    'Petal Width Scaled']
+# Define the prediction bar chart
+fig = px.bar(df_pred, x='Species', y='Confidence',
+             width=350, height=350)
+
+# Create two columns for the web app
+# Column 1 will be for the predictions
+# Column 2 will be for the PCA
+col1, col2 = st.beta_columns((1, 1.5))
+with col1:
+    st.markdown('### Predictions')
+    fig
+
+# Create the PCA chart
+pca_fig = px.scatter(df_pca, x='PC1', y='PC2', color='variety', hover_name='variety', width=500, height=350)
+datapoint = np.array([[
+        sepal_length,
+        sepal_width,
+        petal_length,
+        petal_width
+    ]])
+# Map the 4-D user input to 2-D using the PCA
+datapoint_pca = pca.transform(datapoint)
+# Add the user input to the PCA chart
+pca_fig.add_trace(go.Scatter(
+        x=[datapoint_pca[0, 0]], 
+        y=[datapoint_pca[0,1]], 
+        mode='markers', 
+        marker={'color': 'black', 'size':10}, name='Your Datapoint'))
+
+with col2:
+    st.markdown('### Principle Component Analysis')
+    pca_fig
