2a59b2d570a853a8dddd7c803431bd96b97144fc
Daniel O'Keefe <33761482+DanOKeefe@users.noreply.github.com>
2021-03-27 19:37:20 -0400

    Remove matplotlib import
    matplotlib not used

--- iris_app.py
+++ iris_app.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
 import streamlit as st
-import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
